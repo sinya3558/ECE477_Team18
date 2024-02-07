d45f4ba845fe927e7f6baf164cfb8b117a231d97
choe26 <choe26@purdue.edu>
2024-02-07 16:26:50 -0500

    Add files via upload

---
+++ Bluetooth_Connection/KaraokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bluetooth_ConnectionApp.swift
+//  Bluetooth Connection
+//
+//  Created by Harim Choe on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct KaraokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
